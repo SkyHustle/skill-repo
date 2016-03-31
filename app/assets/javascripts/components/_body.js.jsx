26cca56360c68616a219b1f823e18799a3cd8f78
Dmitry Sky <dmitry.sky@icloud.com>
2016-03-31 16:18:40 -0600

    delete a skill object from the list

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -7,16 +7,34 @@ var Body = React.createClass({
     return { skills: [] }
   },
 
-  handleSubmit(skill){
+  handleSubmit(skill) {
     var newState = this.state.skills.concat(skill);
     this.setState({ skills: newState })
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeSkillFromDOM(id);
+      }
+    });
+  },
+
+  removeSkillFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
   },
 
   render() {
     return (
       <div>
         <NewSkill handleSubmit={this.handleSubmit} />
-        <AllSkills skills={this.state.skills} />
+        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
       </div>
     )
   }
