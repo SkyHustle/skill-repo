864db5576ed4a32ebe6ec4b19543171b2e4e3c0c
Dmitry Sky <dmitry.sky@icloud.com>
2016-04-01 13:59:29 -0600

    update a skill functionality complete

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -30,11 +30,30 @@ var Body = React.createClass({
     this.setState({ skills: newSkills });
   },
 
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: () => {
+        this.updateSkills(skill);
+      }
+    });
+  },
+
+  updateSkills(skill) {
+    var skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
+
   render() {
     return (
       <div>
         <NewSkill handleSubmit={this.handleSubmit} />
-        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
+        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete}
+          handleUpdate={this.handleUpdate}/>
       </div>
     )
   }
